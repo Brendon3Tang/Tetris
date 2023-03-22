eb30b6326ff5aacce8f2f6b8c235c6cec0a084ac
brendon3tang <kyle3tang@gmail.com>
2023-03-22 17:06:31 +0800

    finished comment

--- Tetris/README.md
+++ Tetris/README.md
@@ -1,22 +1,31 @@
-#  <#Title#>
+# 开发日志
 ## 1. M1配置SFML：
-1. 官方Mac配置文档 
+1. 根据官方[Mac配置文档](https://www.sfml-dev.org/tutorials/2.5/start-osx.php)配置。 
 2. Xcode->
     - BuildingSettings-> architecture:改为Standard Architecture(Apple Silicon Intel); Excluded Architectures: 添加arm64（小写）；
     - Building Phases->Run Scripts->For Installs Builds only打勾
 
-## main()
+## 2. 函数，变量介绍
+### 定义二维数组blocks，7行4列，表示有七种形状的俄罗斯方块，每个俄罗斯方块由4个tiles组成。s
 
-## doRotate()
+### 定义二维数组table，20行10列，table为游戏区域。
+
+### main()
+
+### check()
+
+### moveLeftRight(int offset)
+
+### doRotate()
 ![rotate_formula](DevelopLog/rotate_formula.png)
 
-## keyEvent()
+### keyEvent()
 
-## newBlock()
-### - newBlock()把定义好的blocks里的方块根据需要的种类赋予坐标，使他们成为可以放入游戏table的newBlock。
+### newBlock()
+#### - newBlock()把定义好的blocks里的方块根据需要的种类赋予坐标，使他们成为可以放入游戏table的newBlock。
 
 
-## DrawBlock()
+### drawBlock()
 ![drawblock](DevelopLog/drawblock.png)
 - 偏移量：实际的(0, 0)是背景图的左上方，而非背景图中的游戏区域，所以需要设置偏移量。
 1. 已经降落到底部的俄罗斯方块
@@ -32,3 +41,11 @@
         2. setPosition(x, y):  j is column, so j is x; i is row, so i is y
         3. move(x, y): 设置偏移量
         4. window->draw(*spriteTiles); draw it out 
+
+### saveScore()
+
+### initialScore()
+
+### clearLine()
+
+### drop()
--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -51,31 +51,35 @@ int score = 0;
 bool pause = false;
 bool gameOver = false;
 
+// check()会检查当前的俄罗斯方块位置是否合适，如果不合适会返回false
 bool check(){
     for(int i = 0; i < 4; i++){
-        if(curBlock[i].x < 0
-           || curBlock[i].x >= COL_COUNT
-           || curBlock[i]. y >= ROW_COUNT
-           || table[curBlock[i].y][curBlock[i].x] != 0 )
+        if(curBlock[i].x < 0    //超出左边框
+           || curBlock[i].x >= COL_COUNT    //超出右边框
+           || curBlock[i]. y >= ROW_COUNT   //超出下边框
+           || table[curBlock[i].y][curBlock[i].x] != 0 )    //与已存在的方块碰撞
             return false;
     }
     return true;
 }
 
+// moveLeftRight()会根据偏移量将当前方块向左移动或者像右移动
 void moveLeftRight(int offset){
+    if(pause)   return;
+    
     for(int i = 0; i < 4; i++){
         lastBlock[i] = curBlock[i];
         //cout << "offset is: " << offset << endl;
         curBlock[i].x += offset;
     }
-    
+
     if(!check()){
         for(int i = 0; i < 4; i++)
             curBlock[i] = lastBlock[i];
     }
-    
 }
 
+// doRotate()会根据公式将当前俄罗斯方块进行逆时针90度旋转。（由于坐标轴原点在左上角，所以反应在游戏里是顺时针旋转）
 void doRotate(){
     if(blockIndex == 7) return; //如果方块是“田”型，不需要旋转
     Point p = curBlock[1]; //定义旋转中心
@@ -91,11 +95,12 @@ void doRotate(){
     }
 }
 
+//  keyEvent(RenderWindow *window)会根据窗口中发生的事件而做出响应。事件包括左右移动、加速下落、暂停、变形
 void keyEvent(RenderWindow *window){
     bool rotate = false;    //是否旋转
     int offset = 0;
     Event e;    //事件变量
-    
+
     //  pollEvent从事件队列中取出一个事件
     //如果没有事件了，就返回false
     while(window->pollEvent(e)){
@@ -116,10 +121,7 @@ void keyEvent(RenderWindow *window){
                     delay = SPEED_QUICK;
                     break;
                 case Keyboard::Space:
-                    if(pause == true)
-                        pause = false;
-                    else
-                        pause = true;
+                    pause == true ? pause = false : pause = true;
                     break;
                 default:
                     break;
@@ -130,15 +132,15 @@ void keyEvent(RenderWindow *window){
         //处理旋转
         if(rotate)  doRotate();
     }
-    
+
 }
 
 // newBlock（）会把block的序号转化成坐标轴中的坐标，然后赋予curBlock，形成一个新的block
 void newBlock(){
     blockIndex = 1 + rand() % 7;    //从1～7中取一个随机值
-    
+
     int n = blockIndex - 1; //n表示从block[i][j]中取i.index为n（第n+1行）的block
-    
+
     // 把block里的序号转化为坐标轴上的坐标,并且放入curBlock，形成curBlock。
     /*  序号 % 2 = x坐标
      *  序号 / 2 = y坐标
@@ -147,7 +149,7 @@ void newBlock(){
         curBlock[i].x = blocks[n][i] % 2;
         curBlock[i].y = blocks[n][i] / 2;
     }
-    
+
     if(!check()){   //如果生成新方块时就已经不能通过check，那么说明满了，游戏结束
         pause = true;   //  先暂停游戏
         gameOver = true;
@@ -159,6 +161,7 @@ void newBlock(){
     }
 }
 
+// drawBlock会画出当前游戏进程中的block。
 void drawBlock(Sprite *spriteTiles, RenderWindow *window){
     //  1. 已经降落到底部的俄罗斯方块
     for(int i = 0; i < ROW_COUNT; i++){
@@ -174,7 +177,7 @@ void drawBlock(Sprite *spriteTiles, RenderWindow *window){
             }
         }
     }
-    
+
     //  2. 正在降落中的俄罗斯方块（当前方块）只需要画当前的4个blocks
     for(int i = 0; i < 4; i++){ //blockIndex记录了当前方块的种类
         spriteTiles->setTextureRect(IntRect(blockIndex * 18, 0, 18, 18));
@@ -192,7 +195,7 @@ void drop(){
         lastBlock[i] = curBlock[i];
         curBlock[i].y++;
     }
-        
+
     if(!check()){
         //当curBlock到达底部或者与别的block接触时，固化处理
         for(int i = 0; i < 4; i++){
@@ -203,13 +206,14 @@ void drop(){
     }
 }
 
+// clearLine()会把形成一行的方块清除掉
 void clearLine(){
     int k = ROW_COUNT - 1;  //k为慢指针，i为快指针。每当该行的count != col_num时，说明该行不需要消除，k随着i一起往上移动；如果count == col_num时，说明该行需要消除，此时k不上移，当i上移时，“table[k][j] = table[i][j]” 这行代码会将第i行的格子全部copy到第k行（此时k在i的下一行，k也是我们要消除的那一行），这样就相当于把第k行消除了。随着循环，k始终小于i，所以始终会把上一行的格子移动到下一行，所以也完成了上方其余格子的移动
     for(int i = ROW_COUNT-1; i >= 0; i--){
         int count = 0;
         for(int j = 0; j < COL_COUNT; j++){
             if(table[i][j] != 0) count++;
-            
+
             table[k][j] = table[i][j];
         }
         if(count < COL_COUNT){
@@ -221,16 +225,17 @@ void clearLine(){
             score += 10;
         }
     }
-    
+
     char tmp[16];
     sprintf(tmp, "%d", score);
     textScore.setString(tmp);
 }
 
+//initialScore()会初始化分数，倒入历史分数
 void initialScore(){
     if(!font.loadFromFile("/Users/brandon3tang/gameDev/Tetris/Tetris/sansation.ttf"))   exit(1);
     score = 0;
-    
+
     ifstream file(RECORD_FILE);
     if(!file.is_open()){
         cout << RECORD_FILE << "打开失败" << endl;
@@ -239,28 +244,28 @@ void initialScore(){
     else
         file >> highestScore;
     file.close();
-    
+
     textScore.setFont(font);
     textScore.setCharacterSize(20);
     textScore.setColor(Color(0, 153, 153));
     textScore.setStyle(Text::Bold);
     textScore.setPosition(60, 435);
     textScore.setString("0");
-    
+
     scoreTitle.setFont(font);
     scoreTitle.setCharacterSize(20);
     scoreTitle.setColor(Color(0, 153, 153));
     scoreTitle.setStyle(Text::Bold);
     scoreTitle.setPosition(30, 415);
     scoreTitle.setString("SCORE");
-    
+
     historyScore.setFont(font);
     historyScore.setCharacterSize(20);
     historyScore.setColor(Color(0, 153, 153));
     historyScore.setStyle(Text::Bold);
     historyScore.setPosition(200, 435);
     historyScore.setString(to_string(highestScore));
-    
+
     historyScoreTitle.setFont(font);
     historyScoreTitle.setCharacterSize(20);
     historyScoreTitle.setColor(Color(0, 153, 153));
@@ -269,6 +274,7 @@ void initialScore(){
     historyScoreTitle.setString("HISTORY SCORE");
 }
 
+// saveScore会保存当前分数到file中
 void saveScore(){
     if(score > highestScore){
         highestScore = score;
